275b92cd88ed46bf12161c78f95adb1b91f9afc7
akash0631 <akash063@gmail.com>
2026-03-30 18:35:14 +0530

    fix: SharedPreferencesData package — util not commons

--- app/src/main/java/com/v2retail/dotvik/dc/FragmentHUSwapPrint.java
+++ app/src/main/java/com/v2retail/dotvik/dc/FragmentHUSwapPrint.java
@@ -90,7 +90,7 @@ public class FragmentHUSwapPrint extends Fragment implements View.OnClickListene
 
     @Override
     public void onResume() {
-        String _srv = new com.v2retail.commons.SharedPreferencesData(getContext()).read("URL");
+        String _srv = new com.v2retail.util.SharedPreferencesData(getContext()).read("URL");
         com.v2retail.util.PlantNames.load(_srv);
         super.onResume();
         ((Process_Selection_Activity) getActivity())
